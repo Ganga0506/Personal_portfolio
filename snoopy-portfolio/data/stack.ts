749f44b9778bb2474789075ebb6435de4bbd6987
Ganga Nomika Mahesh <ganga.mahesh00@gmail.com>
2026-09-07 17:00:41 -0400

    Start portfolio from scratch

--- snoopy-portfolio/data/stack.ts
+++
@@ -1,8 +0,0 @@
-export const work = [
-    { title: "Research Assistant", description: "ML analysis on metabolic data"},
-  ];
-  
-  export const stack = [
-    { name: "Python", icon: "/images/python.png" },
-    { name: "Next.js", icon: "/images/nextjs.png" },
-  ];
